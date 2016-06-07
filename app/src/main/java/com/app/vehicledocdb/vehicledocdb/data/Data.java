462a76d5579aa982b8ea91bf23c145935c8abc5d
Álvaro Fernández García <alvfergar@gmail.com>
2016-06-07 13:57:06 +0200

    feature (incident default list)

--- app/src/main/java/com/app/vehicledocdb/vehicledocdb/data/Data.java
+++ app/src/main/java/com/app/vehicledocdb/vehicledocdb/data/Data.java
@@ -3,7 +3,9 @@ package com.app.vehicledocdb.vehicledocdb.data;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.app.vehicledocdb.vehicledocdb.greendaomodel.IncidentDao;
 import com.app.vehicledocdb.vehicledocdb.greendaomodel.RequirementDao;
+import com.app.vehicledocdb.vehicledocdb.model.Incident;
 import com.app.vehicledocdb.vehicledocdb.model.Requirement;
 
 import java.util.ArrayList;
@@ -18,16 +20,29 @@ public class Data {
 
     public static void generateDataTest(SQLiteDatabase db){
         Data.db = db;
-        List<ContentValues> contentValuesList = new ArrayList<>();
+        List<ContentValues> contentValuesRequirementList = new ArrayList<>();
+        List<ContentValues> contentValuesIncidentList = new ArrayList<>();
         ContentValues contentValues;
+        
         List<Requirement> requirementList = DataDummy.getInstance().getListRequirements();
+        List<Incident> incidentList = DataDummy.getInstance().getListIncidents();
 
         for(Requirement req: requirementList){
             contentValues = new ContentValues();
             contentValues.put(RequirementDao.Properties.Name.columnName, req.getName());
             contentValues.put(RequirementDao.Properties.EndDate.columnName, req.getEndDate());
-            contentValuesList.add(contentValues);
+            contentValuesRequirementList.add(contentValues);
             db.insert(RequirementDao.TABLENAME, "", contentValues);
+        }
+
+        for(Incident req: incidentList){
+            contentValues = new ContentValues();
+            contentValues.put(IncidentDao.Properties.IncidentName.columnName, req.getIncidentName());
+            contentValues.put(IncidentDao.Properties.Address.columnName, req.getAddress());
+            contentValues.put(IncidentDao.Properties.Date.columnName, req.getDate());
+            contentValues.put(IncidentDao.Properties.Price.columnName, req.getPrice());
+            contentValuesIncidentList.add(contentValues);
+            db.insert(IncidentDao.TABLENAME, "", contentValues);
         }
 
     }

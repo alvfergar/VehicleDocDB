d5bf152e38521d46697552e7cc6f147010a2c1c5
Álvaro Fernández García <alvfergar@gmail.com>
2016-06-07 13:45:02 +0200

    change (dummy dataset) there was an requirement default list and now there are requirement list and incident list too.

--- app/src/main/java/com/app/vehicledocdb/vehicledocdb/data/dataDummy.java
+++ app/src/main/java/com/app/vehicledocdb/vehicledocdb/data/dataDummy.java
@@ -1,5 +1,6 @@
 package com.app.vehicledocdb.vehicledocdb.data;
 
+import com.app.vehicledocdb.vehicledocdb.model.Incident;
 import com.app.vehicledocdb.vehicledocdb.model.Requirement;
 
 import java.util.ArrayList;
@@ -14,13 +15,23 @@ public class DataDummy {
     private static final DataDummy INSTANCE = new DataDummy();
 
     private List<Requirement> listRequirements;
+    private List<Incident> listIncidents;
+
     private DataDummy() {
 
         listRequirements = new ArrayList<Requirement>();
+        listIncidents = new ArrayList<Incident>();
 
         listRequirements.add(new Requirement(null, "Seguro", "2016/08/12"));
         listRequirements.add(new Requirement(null, "Itv", "2017/02/06"));
         listRequirements.add(new Requirement(null, "Itv", "2017/04/15"));
+
+        listIncidents.add(new Incident(null, "Repair", "Calle Aire", "2016/06/05", 59.6));
+        listIncidents.add(new Incident(null, "Inspection", "Av. Socorro", "2016/05/03", 120.60));
+        listIncidents.add(new Incident(null, "Gas", "Calle Becas", "2016/05/03", 20.0));
+        listIncidents.add(new Incident(null, "Gas", "Calle Almansa", "2016/04/03", 50.0));
+        listIncidents.add(new Incident(null, "Gas", "Calle Trujillo", "2016/04/18", 63.5));
+
     }
 
     public static DataDummy getInstance() {
@@ -30,6 +41,11 @@ public class DataDummy {
     public List<Requirement> getListRequirements(){
 
         return listRequirements;
+    }
+
+    public List<Incident> getListIncidents(){
+
+        return listIncidents;
     }
 
 }

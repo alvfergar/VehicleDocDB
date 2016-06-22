4195f209e41e0151cae27d3b9a483779d4b8cfad
Álvaro Fernández García <alvfergar@gmail.com>
2016-06-22 19:54:01 +0200

    feature ( switch onclick and receiver )

--- app/src/main/java/com/app/vehicledocdb/vehicledocdb/RequirementAdapter.java
+++ app/src/main/java/com/app/vehicledocdb/vehicledocdb/RequirementAdapter.java
@@ -1,10 +1,12 @@
 package com.app.vehicledocdb.vehicledocdb;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.app.vehicledocdb.vehicledocdb.model.Requirement;
@@ -15,7 +17,7 @@ import java.util.List;
 /**
  * Created by Alvaro on 05/06/2016.
  */
-public class RequirementAdapter extends ArrayAdapter<Requirement>{
+public class RequirementAdapter extends ArrayAdapter<Requirement> {
 
     private List<Requirement> requirementList;
     private Activity context;
@@ -37,16 +39,30 @@ public class RequirementAdapter extends ArrayAdapter<Requirement>{
 
         String dateToView;
 
-        Requirement requirementInstance = requirementList.get(position);
+        final Requirement requirementInstance = requirementList.get(position);
 
         TextView textRequirementName = (TextView) convertView.findViewById(R.id.textViewRequirementName);
         TextView textRequirementDate = (TextView) convertView.findViewById(R.id.textViewRequirementDate);
+        final Switch switchRequirementAlarm = (Switch) convertView.findViewById(R.id.switchAlarmRequirement);
+
+
+        switchRequirementAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (switchRequirementAlarm.isChecked()) {
+                    Log.wtf("ALARM", "switch activado para alarma " + requirementInstance.getName());
+                }else{
+                    Log.wtf("ALARM", "switch desactivado para alarma " + requirementInstance.getName());
+                }
+            }
+        });
 
         //Date format conversion between DataBase and UI
         dateToView = BuildDates.convertYearMonthDayToDayMonthYear(requirementInstance.getEndDate());
 
         textRequirementName.setText(requirementInstance.getName());
         textRequirementDate.setText(dateToView);
+        switchRequirementAlarm.setChecked(requirementInstance.getEnabled());
 
         return convertView;
     }
